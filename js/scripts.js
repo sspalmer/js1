a1d44ef27b7137022c8d70de84b77d2ae9b6c8a0
samantha <samantha.s.var@gmail.com>
2018-04-09 20:59:28 -0700

    worked on vars and alerts

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var myHistory = "I got bored with regular work and started revisiting my interest in coding. I was entertaining going off to bootcamp in other cities when I came across Geekwise right here in Fresno.";
+//console.log(myHistory);
+//confirm(myHistory);
+var myValue = confirm(myHistory);
+console.log(myValue);
