78b3ce11c821edb42770d95c60c765cefb03d6df
samantha <samantha.s.var@gmail.com>
2018-04-11 00:07:57 -0700

    day1 takehome: log message to console.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,2 @@
-var myHistory = "I got bored with regular work and started revisiting my interest in coding. I was entertaining going off to bootcamp in other cities when I came across Geekwise right here in Fresno.";
-//console.log(myHistory);
-//confirm(myHistory);
-var myValue = confirm(myHistory);
-console.log(myValue);
+var favHobby = "I have a goal to make an app by the end of the year. To prove to myself that I have or can learn the skills to do so. I also need to make more money and investments to set myself up for old age. I hope this class gets me a step closer to both goals while allowing me into a community I'm proud of being a part of. Cooking has been my hobby and always came easy to me. It can be as simple or complex of an art and science as you want it to be. This year though I'm challenging myself and trying my hand at gardening. Wish me luck. ";
+console.log(favHobby);
