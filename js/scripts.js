61a2090aa413ed83ae9dfb2471e98bb1b13fecad
samantha <samantha.s.var@gmail.com>
2018-04-11 21:01:37 -0700

    learned data types, variables, and prompts

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+/*const myName = "Samantha";
+console.log(myName, "one");
+{
+  const myName = "Connie";
+  console.log(myName, "two");
+}
+console.log(myName, "three");
+
+
+let birthDay = '10/14/81';
+let rightNow = new Date(birthDay);
+alert(rightNow);
+
+*/
+
+/*
+//MATH OBJECT
+alert(Math.PI);
+
+//String length
+let myString = "Hello World";
+alert(myString.length);
+
+
+let myArray = ["Sam","Bryan", 3, new Date(), []];
+
+let myObject = {
+  studentCount: 0,
+  teacherRating: 1000000
+}
+*/
+
+//alert
+alert("blah blah blah");
+//confirm
+const isOfAge = confirm("Are you 21 or older?");
+//promt
+const userStory = prompt("Tell me, do you take walks.");
+console.log(userStory);
