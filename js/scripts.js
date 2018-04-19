ffb86e705e283f30cea6190c3833c9cdfd3d67e4
samantha <samantha.s.var@gmail.com>
2018-04-18 18:32:19 -0700

    initial push

--- js/scripts.js
+++ js/scripts.js
@@ -99,7 +99,7 @@ console.log(myNum *= 42); // myNum = myNum * 42
 console.log(myNum /= 42); // myNum = myNum / 42
 */
 
-
+/*
 const best = 42;
 const cheese = "delicious";
 
@@ -110,3 +110,12 @@ if(best === "42"){
 } else {
   console.log("Boooo:");
 };
+*/
+
+// Asks for the user's age using the Date object.
+var birthDate = new Date(prompt("What's your birthdate? yyyy/mm/dd"));
+console.log(birthDate);
+// if the user is over 18, allow them to proceed, otherwise alert the user that they are not old enough.
+// Now that all your users at this point are at least 18, ask if they are less than 80 years old.
+// Now ask the users who are between 18 and 80 if they like Star Wars. If they are over 80, ask if they like prunes.
+// If they say they don't Star Wars, end the program. In all other cases, alert them with a message.
